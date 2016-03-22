7f1b0b3bdab7071d7c0e90822abe952b259999a3
VedeneevGleb <glebvedeneev4@gmail.com>
2016-03-22 21:17:34 +0300

    no message

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField()
+    text = models.CharField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField(User)
+
+
+class Answer(models.Model):
+    text = models.CharField()
+    added_at = models.DateField()
+    question = models.OneToOneField(Question)
+    author = models.ForeignKey(User)
